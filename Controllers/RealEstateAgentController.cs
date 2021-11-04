9d1517c25fce84e98cf4c567bdd6deeb2fa3be41
21michael21 <91207739+21michael21@users.noreply.github.com>
2021-11-04 13:14:27 +0300

    Update RealEstateAgentController.cs

--- Controllers/RealEstateAgentController.cs
+++ Controllers/RealEstateAgentController.cs
@@ -10,29 +10,32 @@ namespace Employment.Controllers
     [ApiController]
     [Route("/RealEstateAgentController")]
     public class RealEstateAgentController : ControllerBase
-
-    [HttpPost("ListOfProperties")]
-     public string ListOfProperties(string str)
     {
+   
+      [HttpPost("ListOfProperties")]
+      public string ListOfProperties(string str)
+     {
         return str;// предоставление списка продающуеся недвижимости
-    }
+     }
 
-    [HttpPost("PermissionToView")]
-    public string PermissionToView(string str)
-    {
+      [HttpPost("PermissionToView")]
+      public string PermissionToView(string str)
+     {
         return str;// получение разрешения на просмотр дома
-    }
+     }
 
-    [HttpPost("RealestateDemonstrations")]
-    public string RealestateDemonstrations(string str)
-    {
+      [HttpPost("RealestateDemonstrations")]
+      public string RealestateDemonstrations(string str)
+     {
         return str;// демонстрация недвижимости
-    }
+     }
 
-    [HttpPost("ConclusionOfTheContract")]
-    public string ConclusionOfTheContract(string str)
-    {
+      [HttpPost("ConclusionOfTheContract")]
+      public string ConclusionOfTheContract(string str)
+     {
         return str;// заключение договора
+     }
+     
     }
 }
    
